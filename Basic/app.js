5c2602037696df432039bfbae29a52db1f690271
1coderbaba <33808010+Bahubuli@users.noreply.github.com>
2024-11-09 20:35:53 +0530

    added clustring, caching, db

--- Basic/app.js
+++ Basic/app.js
@@ -6,9 +6,10 @@ const bodyParser = require('body-parser');
 
 // Use body parser to handle JSON bodies
 app.use(bodyParser.json());
-const urlFile = fs.readFileSync("urls.json",'utf8');
-const urls = JSON.parse(urlFile);
+// const urlFile = fs.readFileSync("urls.json",'utf8');
+// const urls = JSON.parse(urlFile);
 
+const urls = {};
 app.get("/",(req,res)=>{
 res.send(urls)
 })
@@ -18,17 +19,14 @@ app.post("/shorten",(req,res)=>{
 
     const url = req.body.url;
     console.log("url is url",url)
-    // better methods will be use set
-    // or maintain and reverse list of urls to get key
     const exists = Object.values(urls).includes(url);
     if(exists){
-        res.send("url already exists")
+        res.send({data:"url already exists"})
         return;
     }
-    let counter = Object.keys(urls).length + 1; // get the next available key value
-    urls[counter] = url; // add new key-value pair to urls object
-    fs.writeFileSync("urls.json",JSON.stringify(urls)); // write the updated urls object to file
-    res.send(urls);
+    let counter = Object.keys(urls).length + 1;
+    urls[counter] = url;
+    res.send({data:"created successfully"});
 
 })
 
@@ -36,10 +34,10 @@ app.get("/redirect",(req,res)=>{
     const shortUrl = req.query.url;
     const url = urls[shortUrl];
     if(!url){
-        res.send("url not found");
+        res.send({msg:"url not found"});
         return;
     }
-    res.send(url);
+    res.send({data:url});
 })
 
 app.use((err, req, res, next) => {
